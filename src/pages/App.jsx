2e118352846cf39fe938211ed19e36d42ad134ec
GDivino <giodivino.tech@gmail.com>
2022-01-05 22:50:45 +0800

    add navbar

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,11 +1,13 @@
 import Home from './home/index'
 import Community from './community/index'
+import Navigation from './navigation/index'
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 
 const App = () => {
 
   return (
     <Router>
+      <Navigation />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/community' element={<Community />} />
